d6d382a64c791239e5a5c7e1db20cf68ae7ff10a
cookie-monster- <ilive4legos+github@gmail.com>
2019-10-26 17:52:08 -0500

    Initial commit

---
+++ HotDogStandSolution.cpp
@@ -0,0 +1,41 @@
+/*
+Module Name:	Factorial Fibonacci Recursive Overflow
+Date:			11-6-2019
+Author:			Drew Pulliam
+
+Module Purpose
+Track hotdog stand business using classes
+
+Module Design Description
+
+Inputs:
+None
+
+Outputs:
+Unit test of all possible logic paths in the program
+
+*/
+
+#include "pch.h"
+
+#include <safeint.h>
+#include <chrono>
+#include <limits>
+#include <locale>
+
+#include <iostream>
+#include <iomanip>
+
+#include <string>
+
+using namespace std;
+using namespace msl::utilities;
+using namespace chrono;
+
+int main()
+{
+	// formats all numbers >999 to include separating commas
+	cout.imbue(locale(""));
+
+
+}
