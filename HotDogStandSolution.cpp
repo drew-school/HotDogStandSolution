41b46824ecf02557941f438ea67215fa19617836
cookie-monster- <ilive4legos+github@gmail.com>
2019-11-08 12:57:49 -0600

    final assignment

--- HotDogStandSolution.cpp
+++ HotDogStandSolution.cpp
@@ -32,6 +32,7 @@ using namespace std;
 using namespace msl::utilities;
 using namespace chrono;
 
+// declaration of HotDogStandClass
 class HotDogStandClass 
 {
 	private:
@@ -50,9 +51,10 @@ class HotDogStandClass
 		int		getHotDogsSold()	{ return hotDogsSold; };
 		void	hotDogsBuy(int amount);
 		void	stockInventory(int amount);
-		string	display();
+		void	display();
 };
 
+// HotDogStandClass constructor
 HotDogStandClass::HotDogStandClass(int id, string location, double cost, int inventory, int numSold, int * totalSold)
 {
 	standID = id;
@@ -63,6 +65,7 @@ HotDogStandClass::HotDogStandClass(int id, string location, double cost, int inv
 	totalSoldPtr = totalSold;
 }
 
+// hotDogsBuy method, doesn't allow buy > inventory
 void HotDogStandClass::hotDogsBuy(int amount)
 {
 	if (hotDogInventory - amount >= 0)
@@ -73,55 +76,65 @@ void HotDogStandClass::hotDogsBuy(int amount)
 	}
 	else
 	{
+		// tried to buy amount > inventory, print error message
 		cout << "Current inventory is: " << hotDogInventory << endl;
 		cout << "Buy amount requested was: " << amount << endl;
 		cout << "Please retry buying less than " << hotDogInventory << endl;
 	}
 }
 
+// stockInventory method, used to add more hot dog inventory
 void HotDogStandClass::stockInventory(int amount)
 {
 	hotDogInventory += amount;
 }
 
-string HotDogStandClass::display()
+// display debug method, shows the current state of the object
+void HotDogStandClass::display()
 {
-	return "--------------------------------------------------------------\nstandID: " + to_string(standID) + ", standLocation: " + standLocation + ", hotDogCost: " + to_string(hotDogCost) + "\nhotDogInventory: " + to_string(hotDogInventory) + ", hotDogsSold: " + to_string(hotDogsSold) + ", totalSold: " + to_string(*totalSoldPtr);
+	cout << "-----------------------------------------------------------------------------" << endl;
+	cout << "standID: " << standID << setw(29) << "standLocation: " << standLocation << endl;
+	cout << "hotDogInventory: " << hotDogInventory << setw(20) << "hotDogsSold: " << hotDogsSold << endl;
+	cout << "hotDogCost: " << hotDogCost << setw(21) << "totalSold: " << *totalSoldPtr << endl;
+	cout << "-----------------------------------------------------------------------------" << endl << endl;
 }
 
 int totalSold = 0;
 
 int main()
 {
-	// formats all numbers >999 to include separating commas
-	cout.imbue(locale(""));
+	// create three hotDogStand objects with initial values
+	HotDogStandClass hotdogstand1(123, "69 Manhattan St. Rockledge, FL 32955", 2.15, 70, 0, &totalSold);
+	HotDogStandClass hotdogstand2(456, "394 Jackson St. Muskego, WI 53150   ", 3.09, 30, 0, &totalSold);
+	HotDogStandClass hotdogstand3(789, "888 Carson St. Burke, VA 22015      ", 1.95, 15, 0, &totalSold);
 
-	HotDogStandClass hotdogstand1(123, "456 main st", 2.15, 70, 0, &totalSold);
-	HotDogStandClass hotdogstand2(456, "456 main st", 3.00, 30, 0, &totalSold);
-	HotDogStandClass hotdogstand3(789, "456 main st", 1.95, 15, 0, &totalSold);
-
+	// unit test all possible logic paths
+	// test at least one path with each object
 	cout << "initial stand 123" << endl;
-	cout << hotdogstand1.display() << endl << endl;
+	hotdogstand1.display();
 
-	cout << "buy in stock hot dogs from stand 123" << endl;
+	cout << "buy 30 in stock hot dogs from stand 123" << endl;
 	hotdogstand1.hotDogsBuy(30);
-	cout << hotdogstand1.display() << endl << endl;
+	hotdogstand1.display();
 
 	cout << "initial stand 456" << endl;
-	cout << hotdogstand2.display() << endl << endl;
+	hotdogstand2.display();
 
-	cout << "try and buy out of stock hot dogs from stand 456" << endl;
+	cout << "try and buy 35 out of stock hot dogs from stand 456" << endl;
 	hotdogstand2.hotDogsBuy(35);
-	cout << hotdogstand2.display() << endl << endl;
+	hotdogstand2.display();
 
 	cout << "initial stand 789" << endl;
-	cout << hotdogstand3.display() << endl << endl;
+	hotdogstand3.display();
 
-	cout << "restock stand 789" << endl;
-	hotdogstand3.stockInventory(85);
-	cout << hotdogstand3.display() << endl << endl;
+	cout << "restock 75 hot dogs to stand 789" << endl;
+	hotdogstand3.stockInventory(75);
+	hotdogstand3.display();
 
-	cout << "buy in stock hot dogs from stand 789 to show totalSold" << endl;
+	cout << "buy 20 in stock hot dogs from stand 789 to show totalSold increase" << endl;
 	hotdogstand3.hotDogsBuy(20);
-	cout << hotdogstand3.display() << endl << endl;
+	hotdogstand3.display();
+
+	// pause the screen so that information can be read
+	system("pause");	system("cls");
 }
